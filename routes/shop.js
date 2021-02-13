55f61bc98b65f8f2d6098771087679ce24c72a2b
Aleix Badia <aleix.284@gmail.com>
2021-02-13 13:42:23 +0100

    dynamic navbar

--- routes/shop.js
+++ routes/shop.js
@@ -1,49 +1,67 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+function checkLogin(req) {
+  if (req.session.currentUser) {
+    // if user has an authenticated cookie
+    return true;
+  } else {
+    return false;
+  }
+}
+
 /* GET shop pages. */
-router.get('/', function(req, res, next) {
-  res.render('main');
+router.get("/", function (req, res, next) {
+  const logged = checkLogin(req);
+  res.render("main", { logged });
 });
 
-router.get('/products', function(req, res, next) {
+router.get("/products", function (req, res, next) {
+  const logged = checkLogin(req);
   //data missing
-  res.render('shop/gallery'); 
+  res.render("shop/gallery", { logged });
 });
 
-router.get('/products/:designId', function(req, res, next) {
+router.get("/products/:designId", function (req, res, next) {
+  const logged = checkLogin(req);
   //data missing
-  res.render('shop/buy'); 
+  res.render("shop/buy", { logged });
 });
 
-router.get('/vote', function(req, res, next) {
+router.get("/vote", function (req, res, next) {
+  const logged = checkLogin(req);
   //data missing
-  res.render('shop/gallery'); 
+  res.render("shop/gallery", { logged });
 });
 
-router.get('/vote/:designId', function(req, res, next) {
+router.get("/vote/:designId", function (req, res, next) {
+  const logged = checkLogin(req);
   //data missing
-  res.render('shop/vote'); 
+  res.render("shop/vote", { logged });
 });
 
-router.get('/cart', function(req, res, next) {
+router.get("/cart", function (req, res, next) {
+  const logged = checkLogin(req);
   //data missing
-  res.render('shop/cart'); 
+  res.render("shop/cart", { logged });
 });
 
-router.get('/purchase', function(req, res, next) {
+router.get("/purchase", function (req, res, next) {
+  const logged = checkLogin(req);
   //data missing
-  res.render('shop/purchase'); 
+  res.render("shop/purchase", { logged });
 });
 
-router.get('/checkout', function(req, res, next) {
+router.get("/checkout", function (req, res, next) {
+  const logged = checkLogin(req);
   //data missing
-  res.render('shop/checkout'); 
+  res.render("shop/checkout", { logged });
 });
 
-router.get('/user/:userId', function(req, res, next) {
+router.get("/user/:userId", function (req, res, next) {
+  const logged = checkLogin(req);
   //data missing
-  res.render('shop/designer'); 
+  res.render("shop/designer", { logged });
 });
 
 module.exports = router;
