ef135d498d28827cab9c76e49384cc1d2a3f5ca4
Aleix Badia <aleix.284@gmail.com>
2021-02-13 18:20:39 +0100

    gallery v1

--- routes/shop.js
+++ routes/shop.js
@@ -1,6 +1,13 @@
 var express = require("express");
 var router = express.Router();
 
+const Design = require("./../models/designs");
+
+const {getVotes, getVotesRating} = require('./../utils/middleware')
+
+const minVotes = 1;
+const minRating = 1;
+
 function checkLogin(req) {
   if (req.session.currentUser) {
     // if user has an authenticated cookie
@@ -18,8 +25,17 @@ router.get("/", function (req, res, next) {
 
 router.get("/products", function (req, res, next) {
   const logged = checkLogin(req);
-  //data missing
-  res.render("shop/gallery", { logged });
+  Design.find()
+    .then((designs) => {
+      console.log("Get votes: ", getVotes(designs[0]));
+      
+      const data = designs.filter((design) => getVotes(design) > minVotes && getVotesRating(design) > minRating);
+      data.forEach(design => {
+        design.vote = false;
+      });
+      res.render("shop/gallery", { logged, data });
+    })
+    .catch((err) => console.log(err));
 });
 
 router.get("/products/:designId", function (req, res, next) {
@@ -30,8 +46,15 @@ router.get("/products/:designId", function (req, res, next) {
 
 router.get("/vote", function (req, res, next) {
   const logged = checkLogin(req);
-  //data missing
-  res.render("shop/gallery", { logged });
+  Design.find()
+    .then((data) => {
+      data.forEach(design => {
+        design.vote = true;
+      });
+      
+      res.render("shop/gallery", { logged, data });
+    })
+    .catch((err) => console.log(err));
 });
 
 router.get("/vote/:designId", function (req, res, next) {
--- utils/middleware.js
+++ utils/middleware.js
@@ -1,23 +1,37 @@
-function isLoggedIn (req, res, next) {
-    if (req.session.currentUser) { // if user has an authenticated cookie
-      next(); 
-    } 																
-    else {                          	
-        res.redirect("/auth/login");    
-    }                                 
+function isLoggedIn(req, res, next) {
+  if (req.session.currentUser) {
+    // if user has an authenticated cookie
+    next();
+  } else {
+    res.redirect("/auth/login");
   }
-  
-  function isFede(req, res, next) {
-    if (req.session.currentUser.username === 'fede123') {
-      next()
-    }
-    else {
-      next(new Error('You are not Fede!'))
-    }
-  }
-  
-  
-  module.exports = {
-    isLoggedIn,
-    isFede
-  }
+}
+
+function getVotes(design) {
+  const Vote = require("./../models/votes");
+  Vote.find({ designId: design._id })
+    .then((votes) => {
+      return votes.length;
+    })
+    .catch((err) => console.log(err));
+}
+
+function getVotesRating(design) {
+  const Vote = require("./../models/votes");
+  let totalVotes = 0;
+  Vote.find({ designId: design._id })
+    .then((votes) => {
+      console.log(votes);
+      votes.forEach((vote) => {
+        totalVotes += vote.rating
+      });
+      return totalVotes;
+    })
+    .catch((err) => console.log(err));
+}
+
+module.exports = {
+  isLoggedIn,
+  getVotes,
+  getVotesRating,
+};
