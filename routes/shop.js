21ef1fafc201af0f40544f528acf3fed26be5034
Aleix Badia <aleix.284@gmail.com>
2021-02-15 17:46:00 +0100

    products gallery - callback hell solved

--- routes/shop.js
+++ routes/shop.js
@@ -21,30 +21,69 @@ router.get("/", function (req, res, next) {
   res.render("main", { logged });
 });
 
-router.get("/products", function (req, res, next) {
+router.get("/products", async (req, res, next) => {
   const logged = checkLogin(req);
   let rating = 0;
+  let votesByDesign = 0;
   let data = [];
-  Design.find()
-    .then((designs) => {
-      designs.forEach(design => {
-        Vote.find({ designId: design._id })
-          .then( (votesByDesign) => {
-            votesByDesign.forEach(vote => {
-              rating += vote.rating;
-            });
-            console.log(`${design._id} => Votes = ${votesByDesign.length} Rating = ${rating}`);
-            if (votesByDesign.length > minVotes && rating > votesByDesign.length*minRating) {
-              data.push(design)
-            }
-            rating = 0;
-          })
-          .catch( (err) => console.log(err));
+  try {
+    let designsFound = await Design.find();
+    let votesFound = await Vote.find();
+
+    designsFound.forEach((design) => {
+      votesFound.forEach((vote) => {        
+        if (String(design._id) === String(vote.designId)) {          
+          votesByDesign++;
+          rating += vote.rating;
+        }
       });
-      res.render("shop/gallery", { logged, data });
-    })
-    .catch((err) => console.log(err));
+      console.log(
+        `${design._id} => Votes = ${votesByDesign} Rating = ${rating}`
+      );
+      if (
+        votesByDesign > minVotes &&
+        rating > votesByDesign * minRating
+      ) {
+        data.push(design);
+      }
+      rating = 0;
+      votesByDesign = 0;
+    });
+    console.log("Data", data);
+    res.render("shop/gallery", { logged, data });
+  } catch (err) {console.log(err)}
 });
+
+// router.get("/products", function (req, res, next) {
+//   const logged = checkLogin(req);
+//   let rating = 0;
+//   let data = [];
+//   Design.find()
+//     .then((designs) => {
+//       designs.forEach((design) => {
+//         Vote.find({ designId: design._id })
+//           .then((votesByDesign) => {
+//             votesByDesign.forEach((vote) => {
+//               rating += vote.rating;
+//             });
+//             console.log(
+//               `${design._id} => Votes = ${votesByDesign.length} Rating = ${rating}`
+//             );
+//             if (
+//               votesByDesign.length > minVotes &&
+//               rating > votesByDesign.length * minRating
+//             ) {
+//               data.push(design);
+//             }
+//             rating = 0;
+//           })
+//           .catch((err) => console.log(err));
+//       });
+//       console.log("Data", data);
+//       res.render("shop/gallery", { logged, data });
+//     })
+//     .catch((err) => console.log(err));
+// });
 
 router.get("/products/:designId", function (req, res, next) {
   const logged = checkLogin(req);
@@ -56,10 +95,10 @@ router.get("/vote", function (req, res, next) {
   const logged = checkLogin(req);
   Design.find()
     .then((data) => {
-      data.forEach(design => {
+      data.forEach((design) => {
         design.vote = true;
       });
-      
+
       res.render("shop/gallery", { logged, data });
     })
     .catch((err) => console.log(err));
@@ -69,7 +108,7 @@ router.get("/vote/:designId", function (req, res, next) {
   const logged = checkLogin(req);
   Design.findById(req.params.designId)
     .populate("userId")
-    .then((data) => {      
+    .then((data) => {
       res.render("shop/vote", { logged, data });
     })
     .catch((err) => console.log(err));
@@ -96,11 +135,11 @@ router.get("/checkout", function (req, res, next) {
 router.get("/user/:userId", function (req, res, next) {
   const logged = checkLogin(req);
   User.find(req.params.id)
-  .then( (data) => {
-    console.log(data)
-    res.render("shop/designer", { logged, data });
-  })
-  .catch( (err) => console.log(err));
+    .then((data) => {
+      console.log(data);
+      res.render("shop/designer", { logged, data });
+    })
+    .catch((err) => console.log(err));
   //data missing
 });
 
